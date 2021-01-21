df3749dd37d98812c97ea641069e86cf143ca5f3
domagoj.vukovic <off.vukovic@gmail.com>
2021-01-21 18:47:39 +0100

    refactor: fixed react native styles feature: added react native styles

--- react-native.js
+++ react-native.js
@@ -12,8 +12,11 @@ module.exports = {
         }
     },
     "rules": {
-        "no-color-literals": "warn",
-        "no-inline-styles": "error",
-        "no-unused-styles": "error"
+        "react-native/sort-styles": "error",
+        "react-native/no-color-literals": "warn",
+        "react-native/no-inline-styles": "error",
+        "react-native/no-unused-styles": "error",
+        "react-native/no-raw-text": "error",
+        "react-native/no-single-element-style-arrays": "error",
     }
 }
