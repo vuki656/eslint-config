580986e93e2b955906b1b90d99e9bd49744e9ff2
domagoj.vukovic <off.vukovic@gmail.com>
2021-01-21 18:23:03 +0100

    Initial commit

---
+++ react-native.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "env": {
+        "browser": true
+    },
+    "plugins": [
+        "react",
+        "react-native"
+    ],
+    "settings": {
+        "react": {
+            "version": "detect"
+        }
+    },
+    "rules": {
+        "no-color-literals": "warn",
+        "no-inline-styles": "error",
+        "no-unused-styles": "error"
+    }
+}
