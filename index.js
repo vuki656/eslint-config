ee4feecbdf4a6a315850798d00cc63e57392095b
domagoj.vukovic <off.vukovic@gmail.com>
2021-02-23 22:16:46 +0100

    feature: added type-graphql

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports = {
     core: require('./core'),
     react: require('./react'),
     reactNative: require('./react-native'),
-    typescript: require('./typescript')
+    typescript: require('./typescript'),
+    typeGraphql: require('./type-graphql')
 }
